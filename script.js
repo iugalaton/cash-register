59f2ff54e0aeaa8d1f88bc71e6c799b808b4f99b
juliagalaton <iulia.galaton@gmail.com>
2025-01-10 17:26:58 +0100

    Finished

--- script.js
+++ script.js
@@ -6,24 +6,47 @@ const cashier = document.getElementById("change");
 const total = document.getElementById("total");
 
 
-let price = 1.87;
+let price = 44.94;
 let cid = [
-    ['PENNY', 0.5],
-    ['NICKEL', 0],
-    ['DIME', 3.1],
-    ['QUARTER', 4.25],
-    ['ONE', 90],
-    ['FIVE', 55],
-    ['TEN', 20],
-    ['TWENTY', 60],
-    ['ONE HUNDRED', 100]
+    [
+        "PENNY",
+        0.47
+    ],
+    [
+        "NICKEL",
+        0.05
+    ],
+    [
+        "DIME",
+        1.4
+    ],
+    [
+        "QUARTER",
+        1
+    ],
+    [
+        "ONE",
+        1
+    ],
+    [
+        "FIVE",
+        10
+    ],
+    [
+        "TEN",
+        90
+    ],
+    [
+        "TWENTY",
+        100
+    ],
+    [
+        "ONE HUNDRED",
+        900
+    ]
 ];
 
 total.innerHTML = "$" + price;
-
-
-
-
 
 
 class MonetaryUnit {
@@ -106,13 +129,13 @@ class Cashier {
         const restOfChangeReduce = this.cashierDividers
             .filter(divider => divider.monetaryUnit.unit <= change && divider.amount > 0)
             .reduce((acc, divider) => {
-                console.log(divider);
+
                 let times = 0;
                 for (let i = 0; i < divider.amount; i++) {
 
                     if (acc >= divider.monetaryUnit.unit) {
 
-                        console.log(acc);
+
                         acc -= divider.monetaryUnit.unit;
                         times++;
 
@@ -136,49 +159,69 @@ class Cashier {
         if (restOfChangeReduce != 0) {
             return false;
         }
-        console.log(dividersToTakeMoneyFrom);
+
         return dividersToTakeMoneyFrom;
 
     }
 
     removeChange(changeToGive) {
-        changeToGive.forEach(({divider, amount}) => {
+        changeToGive.forEach(({ divider, amount }) => {
             divider.amount -= amount;
         })
+    }
+
+    getStatus() {
+        const fundsInCashier = this.cashierDividers.reduce((acc, divider) => {
+            return acc + divider.amount;
+        }, 0);
+
+        if (fundsInCashier != 0) {
+            return "OPEN";
+        }
+
+        return "CLOSED";
     }
 }
 
 
 //new MonetaryUnit();
 
-const myCashier = new Cashier();
-myCashier.setupCashier(cid);
+
 console.log(myCashier);
 
 const showCid = () => {
     const changeInDrawer = myCashier.cashierDividers.toReversed().map((divider) => {
         return `<p>${divider.monetaryUnit.name}: $${(divider.dividerSum / 100).toFixed(2)}</p>`;
     }).join("");
-    console.log(changeInDrawer);
     cashier.innerHTML = changeInDrawer;
 }
 
 const showChangeToGive = (changeToGive) => {
+    if (changeToGive === false) {
 
-       const info =  changeToGive.map(({divider, amount}) => {
-       console.log(divider, amount);
-       return `<p>${divider.monetaryUnit.type}: $${(amount * divider.monetaryUnit.unit / 100).toFixed(2)}</p>`;
-    
+        changeLocation.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`;
+        return
+    }
+
+    const status = `<p>Status: ${myCashier.getStatus()}</p>`;
+    const info = changeToGive.map(({ divider, amount }) => {
+
+        return `<p>${divider.monetaryUnit.type}: $${(amount * divider.monetaryUnit.unit / 100)}</p>`;
+
     }).join("");
-     changeLocation.innerHTML = info;
+    changeLocation.innerHTML = status + info;
 }
 
 
+const myCashier = new Cashier();
+myCashier.setupCashier(cid);
 
 
 
 const validation = () => {
+    myCashier.setupCashier(cid);
 
+    console.log("CID", cid, "money", input.value, "Price", price)
     const money = Math.floor(input.value * 100);
     const change = money - Math.floor(price * 100);
 
@@ -187,14 +230,19 @@ const validation = () => {
     }
     if (money === Math.floor(price * 100)) {
         const changeInDrawer = `<p> No change due - customer paid with exact cash</p>`
-        changeLocation.innerHTML = changeInDrawer;
+        return changeLocation.innerHTML = changeInDrawer;
     }
 
 
     //  cashier.performTransaction(money, Math.floor(price * 100))
     const changeToGive = myCashier.calculateChange(change);
+
+    if (changeToGive !== false) {    
+        myCashier.removeChange(changeToGive);
+    }
+
     showChangeToGive(changeToGive);
-    myCashier.removeChange(changeToGive);
+
     showCid();
 
 
